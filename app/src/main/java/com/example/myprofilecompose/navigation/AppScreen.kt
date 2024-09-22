90b1300646abec0b7b80343973b69775b2dfaaff
ricar <ricardosr4@gmail.com>
2024-09-21 23:36:11 -0300

    ok...

--- app/src/main/java/com/example/myprofilecompose/navigation/AppScreen.kt
+++ app/src/main/java/com/example/myprofilecompose/navigation/AppScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myprofilecompose.navigation
 
 sealed class AppScreen(val route: String) {
+    //en esta sealed class manejaremos la navegacion
     data object AboutMeScreen : AppScreen("AboutMeScreen")
     data object HomeScreen : AppScreen("HomeScreen")
     data object ProjectScreen : AppScreen("ProjectScreen")
