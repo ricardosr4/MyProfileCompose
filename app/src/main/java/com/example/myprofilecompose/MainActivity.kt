72e52efbaba61ea4d457adde23e076f00d94f73b
ricar <ricardosr4@gmail.com>
2024-09-21 21:28:15 -0300

    creacion de develop

--- app/src/main/java/com/example/myprofilecompose/MainActivity.kt
+++ app/src/main/java/com/example/myprofilecompose/MainActivity.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-
-import androidx.compose.ui.Modifier
-
 import com.example.myprofilecompose.ui.theme.MyProfileComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyProfileComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                }
             }
         }
     }
