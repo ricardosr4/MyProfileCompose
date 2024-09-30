8ddaf7203f1b2e8b1a14325560144093498be220
ricar <ricardosr4@gmail.com>
2024-09-30 00:51:54 -0300

    create home first part

--- app/src/main/java/com/example/myprofilecompose/ui/home/presenter/HomeScreen.kt
+++ app/src/main/java/com/example/myprofilecompose/ui/home/presenter/HomeScreen.kt
@@ -1,34 +1,112 @@
 package com.example.myprofilecompose.ui.home.presenter
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.myprofilecompose.R
 import com.example.myprofilecompose.ui.theme.gradientBrush
 
 @Composable
-fun HomeScreen(){
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(brush = gradientBrush)
-    ){
-        Column(modifier = Modifier
+fun HomeScreen() {
+    val context = LocalContext.current
+
+    Box(
+        modifier = Modifier
             .fillMaxSize()
-            .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
+            .background(brush = gradientBrush)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
-        {
-            Text(text = "Home",
-                fontSize = 26.sp,
-                color = Color.Black)
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                IconButton(onClick = {
+                    val intent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ricardosr4"))
+                    context.startActivity(intent)
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icons_github),
+                        contentDescription = "Github",
+                        modifier = Modifier
+                            .width(50.dp)
+                            .height(50.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+
+                Image(
+                    painter = painterResource(id = R.drawable.img_perfil),
+                    contentDescription = "image profile",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(CircleShape)
+                )
+
+                Spacer(modifier = Modifier.width(20.dp))
+
+                IconButton(onClick = {
+                    val intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://www.linkedin.com/in/ricardosotoramirez/")
+                    )
+                    context.startActivity(intent)
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_linkedin),
+                        contentDescription = "linkedin",
+                        modifier = Modifier
+                            .width(50.dp)
+                            .height(50.dp),
+                        tint = Color.Unspecified
+                    )
+                }
+            }
+
+            Text(
+                text = stringResource(R.string.profile_description),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .padding(top = 30.dp)
+                    .align(Alignment.CenterHorizontally),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
