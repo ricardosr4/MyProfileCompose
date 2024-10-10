03be4b701aed7b0f2e56b7b05c68e3b4a5d51c5d
ricar <ricardosr4@gmail.com>
2024-10-09 23:45:32 -0300

    download cv ok

--- app/src/main/java/com/example/myprofilecompose/ui/aboutme/presenter/AboutMeScreen.kt
+++ app/src/main/java/com/example/myprofilecompose/ui/aboutme/presenter/AboutMeScreen.kt
@@ -1,5 +1,8 @@
 package com.example.myprofilecompose.ui.aboutme.presenter
 
+import android.content.Context
+import android.os.Environment
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -19,7 +22,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,10 +32,15 @@ import androidx.compose.ui.unit.sp
 import com.example.myprofilecompose.R
 import com.example.myprofilecompose.ui.theme.gradientBrush
 import com.example.myprofilecompose.ui.theme.gradientButtonMetallic
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 @Preview(showBackground = true)
 @Composable
 fun AboutMeScreen() {
+    val context = LocalContext.current
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -42,24 +52,22 @@ fun AboutMeScreen() {
                 .align(Alignment.Center),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
-        {
+        ) {
             Image(
                 modifier = Modifier
                     .fillMaxSize()
-                .padding(10.dp),
+                    .padding(10.dp),
                 alignment = Alignment.TopCenter,
                 painter = painterResource(id = R.drawable.img_cv_example),
-                contentDescription = "imagen cv",
-                
-                )
-
+                contentDescription = stringResource(R.string.imagen_cv),
+            )
         }
+
         Button(
-            onClick = { /* Acción */ },
+            onClick = { savePdfToDownloads(context) },
             shape = RoundedCornerShape(20.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent, // Usamos Color.Transparent ya que manejamos el fondo con Brush
+                containerColor = Color.Transparent,
                 contentColor = Color.White
             ),
             modifier = Modifier
@@ -78,17 +86,42 @@ fun AboutMeScreen() {
                 )
         ) {
             Text(
-                text = "Descargar Cv",
+                text = stringResource(R.string.descargar_cv),
                 style = TextStyle(
                     fontSize = 20.sp,
                     color = Color.White,
                     shadow = Shadow(
-                        color = Color.Black, // Sombra del texto
+                        color = Color.Black,
                         blurRadius = 3f
                     )
                 ),
                 modifier = Modifier.align(Alignment.CenterVertically)
             )
         }
+    }
+}
+
+fun savePdfToDownloads(context: Context) {
+    try {
+        val fileName = "cv.ricardo.dev.pdf"
+        val assetManager = context.assets
+        val inputStream: InputStream = assetManager.open(fileName)
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            fileName
+        )
+        val outputStream: OutputStream = FileOutputStream(file)
+        val buffer = ByteArray(1024)
+        var length: Int
+        while (inputStream.read(buffer).also { length = it } > 0) {
+            outputStream.write(buffer, 0, length)
+        }
+        outputStream.flush()
+        outputStream.close()
+        inputStream.close()
+        Toast.makeText(context, "CV guardado en Descargas", Toast.LENGTH_SHORT).show()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Toast.makeText(context, "Error al guardar el CV", Toast.LENGTH_SHORT).show()
     }
 }
